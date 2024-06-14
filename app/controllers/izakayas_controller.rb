687442c12920c3549e32eeeb58bb3adcad7c4aed
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-06-14 23:23:28 +0900

    #13 タグ登録

--- app/controllers/izakayas_controller.rb
+++ app/controllers/izakayas_controller.rb
@@ -3,10 +3,12 @@ class IzakayasController < ApplicationController
 
   def index
     @izakayas = Izakaya.order(id: :asc).all
+    @tags = Tag.order(id: :asc).all
   end
 
   def show
     @izakaya = Izakaya.find(params[:id])
+    @tags = Tag.order(id: :asc).all
     respond_to do |format|
       format.html
       format.turbo_stream
