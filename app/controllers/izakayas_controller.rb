dfad1479e5a7744f3f4db78383211442850c54f4
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-07-05 14:32:16 +0900

    #30 モーダル表示の不具合修正

--- app/controllers/izakayas_controller.rb
+++ app/controllers/izakayas_controller.rb
@@ -6,20 +6,11 @@ class IzakayasController < ApplicationController
     @izakayas = @q.result(distinct: true).includes(:tags, :favorites).order(id: :asc)
     @tags = Tag.order(id: :asc).all
     @plan_exists = current_user ? Plan.exists?(user_id: current_user.id) : false
-
-    respond_to do |format|
-      format.html
-      format.turbo_stream
-    end
   end
 
   def show
     @izakaya = Izakaya.find(params[:id])
     @tags = Tag.order(id: :asc).all
-    respond_to do |format|
-      format.html
-      format.turbo_stream
-    end
   end
 
   def favorites
