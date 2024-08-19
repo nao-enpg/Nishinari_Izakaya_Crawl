58b12e836c4e5f1785a28e360a2c47382151debb
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-08-19 22:44:58 +0900

    #61 Googleフォント導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ud: ['Noto Sans JP', 'Open Sans']
+      }
+    }
+  },
   plugins: [
     require('daisyui')
   ],
