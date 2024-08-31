6cffd1be60bdc4a0a01597b6ba83350c58904425
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-08-31 20:21:37 +0900

    #50 現在地からのレコメンド機能

--- app/controllers/izakayas_controller.rb
+++ app/controllers/izakayas_controller.rb
@@ -16,7 +16,34 @@ class IzakayasController < ApplicationController
       @q = Izakaya.ransack(params[:q])
     end
 
-    @izakayas = @q.result(distinct: true).includes(:tags, :favorites).order(id: :asc)
+    # 現在地からの距離を計算して、10件取得する
+    if params[:lat].present? && params[:lng].present?
+      lat1 = params[:lat].to_f * Math::PI / 180
+      lng1 = params[:lng].to_f * Math::PI / 180
+
+      arounds = Izakaya.all.map do |izakaya|
+        lat2 = izakaya.lat * Math::PI / 180
+        lng2 = izakaya.lng * Math::PI / 180
+
+        diff_y = (lng1 - lng2).abs
+        calc1 = Math.cos(lat2) * Math.sin(diff_y)
+        calc2 = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(diff_y)
+
+        numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
+        denominator = Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(diff_y)
+        degree = Math.atan2(numerator, denominator)
+
+        α = 6378.137
+        distance = degree * α
+
+        { izakaya: izakaya, distance: distance }
+      end
+
+      @izakayas = arounds.sort_by { |h| h[:distance] }.first(10).map { |h| h[:izakaya] }
+    else
+      @izakayas = @q.result(distinct: true).includes(:tags, :favorites).order(id: :asc)
+    end
+
     @plan_exists = current_user ? Plan.exists?(user_id: current_user.id) : false
   end
 
